60bce7d20d781bf981fe827154432e0198ce96ee
HrithikVaya007 <hrithikvay3004@gmail.com>
2026-07-21 15:21:30 +0530

    New changes to finish deployment for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: true // Allows Render (and any other domain) to access the preview server
+  }
 })
